bd66b1ad648de1432af56f4242bfb825c27cca4d
xXJSONDeruloXx <danielhimebauch@gmail.com>
2026-06-02 12:24:13 -0400

    feat(decomp): Decompile empty float argument stub func_151348F0

--- conker/src/game_161520.c
+++ conker/src/game_161520.c
@@ -90,7 +90,9 @@ void func_1513477C(struct102 *arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_161520/func_151347CC.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game_161520/func_151348F0.s")
+void func_151348F0(f32 arg0, f32 arg1, s32 arg2, s32 arg3) {
+}
+
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_161520/func_15134908.s")
 
