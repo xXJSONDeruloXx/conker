22b0d66e19699afb5b433f99ee7c70d3c3ceaf93
xXJSONDeruloXx <danielhimebauch@gmail.com>
2026-06-02 13:28:43 -0400

    feat(decomp): Decompile variadic callback setup func_10002088

--- conker/src/init_2070.c
+++ conker/src/init_2070.c
@@ -3,12 +3,18 @@
 #include "functions.h"
 #include "variables.h"
 
+void func_100020D0(s32 (*arg0)(s32, s32, s32), s32 arg1, s32 arg2, s32 *arg3);
+
 
 s32 func_10002070(s32 arg0, s32 arg1, s32 arg2) {
     return 1;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/init_2070/func_10002088.s")
+void func_10002088(s32 arg0, ...) {
+    D_80035500 = 0;
+    func_100020D0(func_10002070, 0, arg0, &arg0 + 1);
+}
+
 // NOT MATCHING: stack is incorrect
 // void func_10002088(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
 //     D_80035500 = 0;
