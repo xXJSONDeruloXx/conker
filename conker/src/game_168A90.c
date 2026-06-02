c203fa7a607498a6d000e6032dfa6266471d6a0e
xXJSONDeruloXx <danielhimebauch@gmail.com>
2026-06-02 14:51:01 -0400

    feat(decomp): Decompile func_1513BA78: switch on unk48 dispatching to two different handler functions

--- conker/src/game_168A90.c
+++ conker/src/game_168A90.c
@@ -3,6 +3,9 @@
 #include "functions.h"
 #include "variables.h"
 
+void func_15109064(struct132 *, s32, u8);
+void func_151BA468(struct132 *, s32, u8);
+
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_168A90/func_1513B5E0.s")
 #pragma GLOBAL_ASM("asm/nonmatchings/game_168A90/func_1513B798.s")
@@ -32,7 +35,16 @@ void func_1513BA44(struct132 *arg0) {
     D_80089C54[arg0->unk48]();
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game_168A90/func_1513BA78.s")
+void func_1513BA78(struct132 *arg0, s32 arg1, u8 arg2) {
+    switch (arg0->unk48) {
+        case 1:
+            func_15109064(arg0, arg1, arg2);
+            break;
+        case 2:
+            func_151BA468(arg0, arg1, arg2);
+            break;
+    }
+}
 
 s32 func_1513BAD4(s32 arg0, s32 arg1) {
     return 0;
