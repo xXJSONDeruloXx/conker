728809d491746abfcadd0ff1b81bcf430d6eacb2
xXJSONDeruloXx <danielhimebauch@gmail.com>
2026-06-02 15:13:22 -0400

    feat(decomp): match func_151F2BE8

--- conker/src/libultra/audio/game_21FC90.c
+++ conker/src/libultra/audio/game_21FC90.c
@@ -79,7 +79,16 @@ void func_151F2BA8(void) {
     osSetIntMask(mask);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/audio/game_21FC90/func_151F2BE8.s")
+void func_151F2BE8(void) {
+    u32 mask = osSetIntMask(1);
+
+    if (D_800E0E04 == 5) {
+        D_800E0E04 = 6;
+    } else {
+        D_800E0E04 = 2;
+    }
+    osSetIntMask(mask);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra/audio/game_21FC90/func_151F2C4C.s")
 
