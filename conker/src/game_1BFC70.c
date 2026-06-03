02ef02180403e2bcd0930ef8e7f993c484b1d24d
xXJSONDeruloXx <danielhimebauch@gmail.com>
2026-06-03 02:31:22 -0400

    feat(decomp): Decompile HUD meter reset helper

--- conker/src/game_1BFC70.c
+++ conker/src/game_1BFC70.c
@@ -3,7 +3,17 @@
 #include "variables.h"
 
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game_1BFC70/func_151927C0.s")
+void func_151927C0(struct114 *arg0) {
+    s32 tmp0;
+    s32 tmp1;
+
+    tmp0 = (s16)((arg0->unk14 & 0xFF) >> 1);
+    tmp1 = tmp0 << 16;
+    arg0->unk38 = 300;
+    arg0->unk3A = 10;
+    arg0->unk14 = tmp1 + tmp0;
+    arg0->unk3B = 0;
+}
 // JUSTREG: using t2 not t3
 // void func_151927C0(struct114 *arg0) {
 //     s32 tmp0;
