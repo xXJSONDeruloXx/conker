2896bda9a8180a27da585df15808368c20555547
xXJsonDeruloXx <danielhimebauch@gmail.com>
2026-08-05 02:51:54 -0400

    Match func_1513B83C
    Co-authored-by: ssssylassss <ssssylassss@gmail.com>

--- conker/src/game_168A90.c
+++ conker/src/game_168A90.c
@@ -6,10 +6,39 @@
 void func_15109064(struct132 *, s32, u8);
 void func_151BA468(struct132 *, s32, u8);
 
+typedef struct {
+    u8  pad0[0x10];
+    u8  unk10;
+    u8  pad11[0x1];
+    s8  unk12;
+    u8  pad13[0x36];
+    u8  unk49;
+    u8  pad4A[0xA];
+    s32 unk54;
+} Obj168A90B;
+
+extern s32 (*D_80089C28[])(Obj168A90B *, s32);
+
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_168A90/func_1513B5E0.s")
 #pragma GLOBAL_ASM("asm/nonmatchings/game_168A90/func_1513B798.s")
-#pragma GLOBAL_ASM("asm/nonmatchings/game_168A90/func_1513B83C.s")
+Gfx *func_1513B83C(Gfx *arg0, Obj168A90B *arg1, s16 arg2) {
+    s8 v0;
+
+    if (((arg1->unk10 & 2) != 0) && ((arg1->unk49 & (1 << arg2)) == 0)) {
+        return arg0;
+    }
+    v0 = arg1->unk12;
+    if (v0 != -1) {
+        if (D_80089C28[v0](arg1, (arg2 << 0x10) >> 0x10) == 0) {
+            return arg0;
+        }
+    }
+    { Gfx *g = arg0++; g->words.w0 = 0xDA380003; g->words.w1 = (u32)((u8 *)arg1 + (D_800BE9C0 << 6) + 0x78); }
+    { Gfx *g = arg0++; g->words.w0 = 0xDB060004; g->words.w1 = *(u32 *)((u8 *)arg1 + (D_800BE9C0 << 4) + (arg2 << 2) + 0x58); }
+    { Gfx *g = arg0++; g->words.w0 = 0xDE000000; g->words.w1 = arg1->unk54; }
+    return arg0;
+}
 
 s32 func_1513B968(s32 arg0, s32 arg1) {
     // FIXME: &arg0->unk_120[D_800BE9C0]
